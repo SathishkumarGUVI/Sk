92bbd18bd469f0aa37cd10cf499f1a2223a3c21a
SathishkumarGUVI <53076605+SathishkumarGUVI@users.noreply.github.com>
2019-07-20 22:32:34 +0530

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,19 @@
+s=input()
+k=0
+l=0
+v=['a','e','i','o','u']
+c=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+for j in range(21):
+  if(c[j]==str(s)):
+     print("Consonant")
+     break
+  else:
+     k+=1
+for i in range(5):
+  if(v[i]==s):
+     print("Vowel")
+     break
+  else:
+     l+=1
+if(k==21 and l==5):
+  print("Invalid")
