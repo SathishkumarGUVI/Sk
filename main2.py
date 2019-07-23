08826d1b04fb2d537c350ed45f6e9badfb0783b2
SathishkumarGUVI <53076605+SathishkumarGUVI@users.noreply.github.com>
2019-07-23 20:48:38 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,16 +1,16 @@
-s=input()
+string=input()
 k=0
 l=0
 v=['a','e','i','o','u']
 c=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
 for j in range(21):
-  if(c[j]==str(s)):
+  if(c[j]==string):
      print("Consonant")
      break
   else:
      k+=1
 for i in range(5):
-  if(v[i]==s):
+  if(v[i]==string):
      print("Vowel")
      break
   else:
