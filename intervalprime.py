8e65dd816086feccada25d154d068237d6ca3d5b
SathishkumarGUVI <53076605+SathishkumarGUVI@users.noreply.github.com>
2019-08-24 12:31:37 +0530

    Create intervalprime.py

---
+++ intervalprime.py
@@ -0,0 +1,14 @@
+lower,upper =input().split()
+lower=int(lower)
+upper=int(upper)
+upper=upper+1
+st=[]
+for num in range(lower,upper-1):
+      if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           st.append(str(num))
+
+print(" ".join(st))
